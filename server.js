0fff58d8f23b838f7111a7097171baa7e40c29eb
Rogerio Rodrigues <sp.rogerio@outlook.com>
2019-11-13 03:13:26 -0300

    WebSocket Project

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express'); 
+const path = require('path'); 
+const PORTA = 3000;
+
+const app = express(); 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html'); 
+
+app.use('/', (req, res) =>{
+    res.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', socket =>{
+    console.log(`Socket conectado: ${socket.id}`);
+
+    if(messages)
+    socket.emit('previousMessages', messages);
+
+    socket.on('sendMessage', data =>{
+        
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+        
+        //console.log(data);
+    })
+})
+
+server.listen(PORTA, (err) =>{
+    console.log(`Aplicação rodando na porta ${PORTA}`)
+});
+
